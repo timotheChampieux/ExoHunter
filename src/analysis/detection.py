d35239cec0d893c1f2bac691b896dce70af9991b
Timothe <timothe.champieux@ynov.com>
2026-03-16 14:23:09 +0100

    feat: ajout d'une fonction _run_bls_analysis permetant d'exterioriser la recherche de planète afin de liberer le fonction principale(planete detector)

--- src/analysis/detection.py
+++ src/analysis/detection.py
@@ -13,10 +13,34 @@ def _get_search_params(lc : lk.LightCurve) :
     steps = int(observation_time * 500)
 
     if max_period < min_period :
-        return max_period+=min_period,steps,min_period
+        return max_period+min_period,steps,min_period
 
     return max_period,steps,min_period 
+
+def _run_bls_analysis(lc) : 
+    max_p, steps,min_p  = _get_search_params(lc)
+
+    periods =  np.linspace(min_p,max_p,steps)
+    bls = lc.to_periodogram(method='bls',period=periods)
     
+    best_period = bls.period_at_max_power
+    best_t0 = bls.transit_time_at_max_power
+    best_duration = bls.duration_at_max_power
+
+    stats = bls.compute_stats(period=best_period, 
+                            duration=best_duration, 
+                            transit_time=best_t0)
+
+    snr = stats['snr']
+
+    result = {
+        "period": best_period.value,
+        "transit_time": best_t0.value,
+        "duration": best_duration.value,
+        "snr": snr,
+        "max_power": bls.max_power.value
+    }
+    return result
 def _mask_planet(lc, planet_info) :
 
     period,transit_time,duration = planet_info["period"],planet_info["transit_time"],planet_info["duration"]
@@ -33,30 +57,9 @@ def planet_detector(lc : lk.LightCurve, max_planets=10) :
 
     while len(planets_found)<max_planets : 
 
-        max_p, steps,min_p  = _get_search_params(lc)
-
-        periods =  np.linspace(min_p,max_p,steps)
-        bls = current_lc.to_periodogram(method='bls',period=periods)
+        result = _run_bls_analysis(current_lc)
         
-        best_period = bls.period_at_max_power
-        best_t0 = bls.transit_time_at_max_power
-        best_duration = bls.duration_at_max_power
-
-        stats = bls.compute_stats(period=best_period, 
-                                duration=best_duration, 
-                                transit_time=best_t0)
-
-        snr = stats['snr']
-
-        result = {
-            "period": best_period.value,
-            "transit_time": best_t0.value,
-            "duration": best_duration.value,
-            "snr": snr,
-            "max_power": bls.max_power.value
-        }
-        
-        if snr > 7 : 
+        if result["snr"] > 7 : 
             planets_found.append(result)
             current_lc = _mask_planet(current_lc,result)
         else : 
