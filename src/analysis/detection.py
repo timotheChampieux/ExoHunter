a13c4c441e2d9d58eba114d9c69ae87e92e72eec
Timothe <timothe.champieux@ynov.com>
2026-03-16 10:58:06 +0100

    feat: implementation de la boucle permetant de trouver plusieurs planete dans planet detector

--- src/analysis/detection.py
+++ src/analysis/detection.py
@@ -21,36 +21,48 @@ def _mask_planet(lc, planet_info) :
 
     period,transit_time,duration = planet_info["period"],planet_info["transit_time"],planet_info["duration"]
 
-    masque = lc.create_transit_mask(period=period, transit_time=transit_time*2, duration=duration)
-    
+    masque = lc.create_transit_mask(period=period, transit_time=transit_time, duration=duration*3)
+
     lc_propre = lc[~masque]
     return lc_propre
 
-def planet_detector(lc : lk.LightCurve) : 
+def planet_detector(lc : lk.LightCurve, max_planets=10) : 
 
-    max_p, steps,min_p  = _get_search_params(lc)
+    planets_found = []
+    current_lc = lc
 
-    periods =  np.linspace(min_p,max_p,steps)
-    bls = lc.to_periodogram(method='bls',period=periods)
-    
-    best_period = bls.period_at_max_power
-    best_t0 = bls.transit_time_at_max_power
-    best_duration = bls.duration_at_max_power
+    while len(planets_found)<max_planets : 
 
-    stats = bls.compute_stats(period=best_period, 
-                              duration=best_duration, 
-                              transit_time=best_t0)
+        max_p, steps,min_p  = _get_search_params(lc)
 
-    snr = stats['snr']
+        periods =  np.linspace(min_p,max_p,steps)
+        bls = current_lc.to_periodogram(method='bls',period=periods)
+        
+        best_period = bls.period_at_max_power
+        best_t0 = bls.transit_time_at_max_power
+        best_duration = bls.duration_at_max_power
 
-    result = {
-        "period": best_period.value,
-        "transit_time": best_t0.value,
-        "duration": best_duration.value,
-        "snr": snr,
-        "max_power": bls.max_power.value
-    }
-    
-    return result
+        stats = bls.compute_stats(period=best_period, 
+                                duration=best_duration, 
+                                transit_time=best_t0)
+
+        snr = stats['snr']
+
+        result = {
+            "period": best_period.value,
+            "transit_time": best_t0.value,
+            "duration": best_duration.value,
+            "snr": snr,
+            "max_power": bls.max_power.value
+        }
+        
+        if snr > 7 : 
+            planets_found.append(result)
+            current_lc = _mask_planet(current_lc,result)
+        else : 
+            return planets_found
+
+
+      
 
 
